44e853ce1cb478df899632f81de8824fa3d82087
JDavidYY <yonjosedavid@gmail.com>
2020-02-12 00:29:38 -0500

    version 0.0.2

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById('formTask').addEventListener('submit',function(e){
+    
+    e.preventDefault();
+    alert("Enviando formulario");
+    console.log("hola mundo mundial");
+});
+
