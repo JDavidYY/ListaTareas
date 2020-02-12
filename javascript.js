17339857a1c451fa81941c67f0f514814f6d16ce
JDavidYY <yonjosedavid@gmail.com>
2020-02-12 02:17:39 -0500

    version 0.0.3

--- javascript.js
+++ javascript.js
@@ -1,7 +1,30 @@
-document.getElementById('formTask').addEventListener('submit',function(e){
+document.getElementById('formTask').addEventListener('submit', guardarTarea);
     
-    e.preventDefault();
-    alert("Enviando formulario");
-    console.log("hola mundo mundial");
-});
+    function guardarTarea(e){
+        
+        alert("Enviando formulario");
+        let title = document.getElementById('title').value;
+        let description = document.getElementById('description').value;
+        const task={
+            title,
+            description
+        };
+      //  localStorage.setItem('tasks',JSON.stringify(task))
+       
+        if(localStorage.getItem("tasks") === null){
+            let tasks=[]
+            tasks.push(task)
+            localStorage.setItem('tasks',JSON.stringify(tasks))
+            console.log("kha?")
+        } else{
+           let tasks =  JSON.parse(localStorage.getItem('tasks'))
+           tasks.push(task)
+           localStorage.setItem('tasks',JSON.stringify(tasks))
+        }
+        
+        e.preventDefault();  
+    }
+    //console.log("hola mundo mundial");
+    //console.log(title, description)
 
+//console.log(localStorage.setItem('tasks',task))
