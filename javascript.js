9fde5fc0917a4c6452a81bb722627d615675468e
JDavidYY <yonjosedavid@gmail.com>
2020-02-16 01:44:11 -0500

    version 0.0.5

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 document.getElementById('formTask').addEventListener('submit', guardarTarea);
+document.getElementById('formTask').addEventListener('submit', getTask);
     
     function guardarTarea(e){
         
@@ -21,23 +22,43 @@ document.getElementById('formTask').addEventListener('submit', guardarTarea);
            tasks.push(task)
            localStorage.setItem('tasks',JSON.stringify(tasks))
         }
-        
+        getTask();
+        document.getElementById('tasks').reset();
         e.preventDefault();  
     }
     function getTask(){
         let tasks = JSON.parse(localStorage.getItem('tasks'))
         let tasksView = document.getElementById('tasks')
 
+        tasksView.innerHTML='';
 
         for (i=0;i<tasks.length;i++){
-            tasksView.innerHTML += `<div class="card">
+            tasksView.innerHTML += `<div class="card mb-3">
                 <div class="body-card">
                    <p> ${tasks[i].title} - ${tasks[i].description} </p>
-                   <a class="btn btn-block btn btn-danger "> Tarea realizada </a>
+                   <a class="btn btn-block btn btn-danger" onclick="eliminarTarea('${tasks[i].title}')"> Elimminar tarea </a>
                 </div>
                 </div>`
         }
     }
+
+    function eliminarTarea(a){
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        for (let i=0;i<tasks.length;i++){
+            console.log(a)
+           // console.log(tasks[i].title)
+            //let title = tasks[i].title;
+            //let description = tasks[i].description;
+            if(tasks[i].title == a){
+                console.log("Gaaa")
+                tasks.splice(i,1);
+            }
+        
+        }
+        localStorage.setItem('tasks',JSON.stringify(tasks));
+        getTask();
+    }
+
     //console.log("hola mundo mundial");
     //console.log(title, description)
 
